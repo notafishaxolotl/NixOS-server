c11354c62d942169ac157b1531a38ea0bcc166a1
notafishaxolotl <116303535+notafishaxolotl@users.noreply.github.com>
2025-12-12 14:35:18 +0000

    Update unstable.nix

--- nixos/unstable.nix
+++ nixos/unstable.nix
@@ -1,1 +1,17 @@
+{ config, pkgs, pkgs-unstable, ... }:
 
+{
+  
+  nixpkgs.config = {
+    allowUnfree = true;
+    packageOverrides = pkgs: {
+      unstable = import <nixpkgs> {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+      unstable.davinci-resolve
+    ];
+}
