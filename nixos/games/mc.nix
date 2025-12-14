8e4abc92b04ff53b70f989862127bba0b0eec4e2
notafishaxolotl <116303535+notafishaxolotl@users.noreply.github.com>
2025-12-14 21:12:01 +0000

    Update mc.nix

--- nixos/games/mc.nix
+++ nixos/games/mc.nix
@@ -4,6 +4,23 @@
   services.minecraft-server = {
   enable = true;
   eula = true;
+  openFirewall = true; # Opens the port the server is running on (by default 25565 but in this case 43000)
+  declarative = true;
+  whitelist = {
+    # This is a mapping from Minecraft usernames to UUIDs. You can use https://mcuuid.net/ to get a Minecraft UUID for a username
+    username1 = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+    username2 = "yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy";
   };
+  serverProperties = {
+    server-port = 43000;
+    difficulty = 3;
+    gamemode = 1;
+    max-players = 5;
+    motd = "NixOS Minecraft server!";
+    white-list = true;
+    allow-cheats = true;
+  };
+  jvmOpts = "-Xms2048M -Xmx4096M";
+};
 
 }
