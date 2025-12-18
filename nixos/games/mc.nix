1a311569a43c79d4a8a9fde1e9b51426ef43fdf1
notafishaxolotl <116303535+notafishaxolotl@users.noreply.github.com>
2025-12-18 09:49:03 +0000

    Update mc.nix

--- nixos/games/mc.nix
+++ nixos/games/mc.nix
@@ -15,11 +15,17 @@
   eula = true;
   openFirewall = true;
   declarative = true;
-  
-  servers.WuluH.whitelist = {
-  xr_lynx = "b55d65b2-3aea-4a29-968c-0a405b5a9d26";
+
+  servers.WuluH.serverProperties = {
+    server-port = 43000;
+    difficulty = 2;
+    gamemode = 0;
+    max-players = 5;
+    motd = "WuluH";
+    white-list = true;
+    allow-cheats = false;
   };
-  
+
   servers.WuluH.operators = {
     # Use https://mcuuid.net/ to get a Minecraft UUID for a username
     xr_lynx = {
@@ -28,16 +34,12 @@
       bypassesPlayerLimit = true;
     };
   };
-  serverProperties = {
-    server-port = 43000;
-    difficulty = 3;
-    gamemode = 1;
-    max-players = 5;
-    motd = "WuluH";
-    white-list = true;
-    allow-cheats = false;
+
+  servers.WuluH.whitelist = {
+  xr_lynx = "b55d65b2-3aea-4a29-968c-0a405b5a9d26";
   };
-  jvmOpts = "-Xms2048M -Xmx4096M";
-};
+  
+  
+  
 
 }
